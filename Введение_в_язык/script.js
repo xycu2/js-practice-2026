fee808c546e07424bb6a7fece899c183a81aa028
xycu <smonindemid@gmail.com>
2026-07-01 19:52:13 +0500

    add varbls and console.log in script.js

--- Введение_в_язык/script.js
+++ Введение_в_язык/script.js
@@ -1,1 +1,9 @@
-console.log(1)
+const title = "title";
+const screens = "screens";
+const screenPrice = "screenPrice";
+const rollback = "rolback";
+const fullPrice = "fullPrice";
+const adaptive = "adaptive";
+
+alert("Hello, javaScript!")
+console.log("hello, world!")
