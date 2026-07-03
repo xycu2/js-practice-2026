a0e6ddcf7e3ffd807eed7ba6dad39de514b5fdd2
xycu <smonindemid@gmail.com>
2026-07-03 15:30:33 +0500

    add function getFullPrice

--- function_Урок_4/hwEasy/script.js
+++ function_Урок_4/hwEasy/script.js
@@ -3,3 +3,10 @@ const getAllServicePrices = function(service, service1) {
     return service + service1;
 }
 const allServicePrices = getAllServicePrices(500, 600);
+
+
+// №2
+function getFullPrice(screenPrice, allServicePrices) {
+    return screenPrice + allServicePrices;
+}
+const fullPrice = getFullPrice(150, 450);
