9db1b27426703d02377face11b607cc151dd1263
xycu <smonindemid@gmail.com>
2026-07-06 14:35:13 +0500

    Days of the week task

--- array_Урок_7/script.js
+++ array_Урок_7/script.js
@@ -1,7 +1,25 @@
 'use strict'
 
-const week = ['Понеделник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье'];
+const week = ['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье'];
 
 // №1
 console.log(week);
 week.forEach(day => console.log(day));
+
+// №2
+week.forEach((day, index) => {
+
+    const currentDay = new Date().getDay();
+
+    let jsDayIndex = index + 1;
+    if (jsDayIndex === 7) jsDayIndex = 0;
+
+    if (jsDayIndex === currentDay) {
+        document.write(`<b>${day}</b><br>`);
+    } else if (day === 'Суббота' || day === 'Воскресенье') {
+        document.write(`<i>${day}</i><br>`);
+    } else {
+        document.write(`${day}<br>`);
+    }
+})
+
