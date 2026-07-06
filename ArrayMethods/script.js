60a70432d046c851fd1404fb90b5731bd05fc8c9
xycu <smonindemid@gmail.com>
2026-07-06 14:57:30 +0500

    method concat

---
+++ ArrayMethods/script.js
@@ -0,0 +1,16 @@
+const books = [
+    {id: 1, price: 500},
+    {id: 2, price: 1000}
+]
+
+// №1 concat
+// const newArr = books.concat({id: 3, price: 450});
+// console.log(newArr)
+
+/*
+{ id: 1, price: 500 },
+{ id: 2, price: 1000 },
+{ id: 3, price: 450 }
+*/
+
+
