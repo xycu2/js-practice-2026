2feaade51985ef0e5e9f2b33b438fe2988bee3b1
xycu <smonindemid@gmail.com>
2026-07-06 15:01:46 +0500

    method slice

--- ArrayMethods/script.js
+++ ArrayMethods/script.js
@@ -1,6 +1,8 @@
 const books = [
     {id: 1, price: 500},
-    {id: 2, price: 1000}
+    {id: 2, price: 1000},
+    {id: 3, price: 234},
+    {id: 4, price: 780}
 ]
 
 // №1 concat
@@ -14,3 +16,12 @@ const books = [
 */
 
 
+// №2 slice(start, end) end - не включая
+// const newArr = books.slice(0, 3) не включая 3 индекс
+// console.log(newArr)
+
+/*
+{ id: 1, price: 500 },
+{ id: 2, price: 1000 },
+{ id: 3, price: 234 }
+*/
