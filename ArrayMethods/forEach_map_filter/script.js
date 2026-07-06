1935a6b0aaa34ba0b9344f341f0063dd1b49d8f1
xycu <smonindemid@gmail.com>
2026-07-06 20:43:28 +0500

    method filter

--- ArrayMethods/forEach_map_filter/script.js
+++ ArrayMethods/forEach_map_filter/script.js
@@ -2,7 +2,8 @@ const books = [
     {id: 0, price: 500},
     {id: 1, price: 1000},
     {id: 2, price: 234},
-    {id: 3, price: 780}
+    {id: 3, price: 780},
+    {id: 4, price: 500}
 ];
 
 // ==================================
@@ -30,3 +31,14 @@ const books = [
 // console.log('map:')
 // console.log(newArr); выведится новый массив
 // console.log(books); выведится изначальный массив
+
+
+// ==================================
+// filter (создет новые массив, туда пройдут только те элементы , которые пройдут условие)
+// ==================================
+
+// const newArr = books.filter(item => {
+//     return item.price === 500;
+// })
+
+// console.log(newArr) // [ { id: 0, price: 500 }, { id: 4, price: 500 } ]
