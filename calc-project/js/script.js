53a79de5d93996780a82681948a2181cb7582016
xycu <smonindemid@gmail.com>
2026-07-07 14:37:18 +0500

    find buttons on the page

--- calc-project/js/script.js
+++ calc-project/js/script.js
@@ -1,4 +1,9 @@
 //  №1
 const title = document.getElementsByTagName('h1');
 
-console.log(title[0])
+console.log(title[0]);
+
+// №2
+const headerBtn = document.getElementsByClassName('handler_btn');
+
+console.log(headerBtn);
