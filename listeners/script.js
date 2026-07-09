8199e7cecb89b9c2342088430a249b5135880025
xycu <smonindemid@gmail.com>
2026-07-09 17:11:40 +0500

    hide the button inside the circle

--- listeners/script.js
+++ listeners/script.js
@@ -2,9 +2,13 @@
 const btn = document.getElementById('btn');
 const input = document.getElementById('text');
 const square = document.getElementById('square');
+const btnInSquare = document.getElementById('e_btn');
 
 btn.addEventListener('click', () => {
     const colorSquare = input.value;
 
     square.style.backgroundColor = `${colorSquare}`;
 })
+
+// №2
+btnInSquare.style.display = 'none';
