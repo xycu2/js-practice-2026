70d6cab4685c5a85155194b2677358d55bff1324
xycu <smonindemid@gmail.com>
2026-07-09 17:18:54 +0500

    Make the circle's width and height change when the slider's position is changed

--- listeners/script.js
+++ listeners/script.js
@@ -2,7 +2,9 @@
 const btn = document.getElementById('btn');
 const input = document.getElementById('text');
 const square = document.getElementById('square');
+const circle = document.getElementById('circle');
 const btnInSquare = document.getElementById('e_btn');
+const inputRange = document.getElementById('range');
 
 btn.addEventListener('click', () => {
     const colorSquare = input.value;
@@ -12,3 +14,10 @@ btn.addEventListener('click', () => {
 
 // №2
 btnInSquare.style.display = 'none';
+
+// №3
+
+inputRange.addEventListener('input', () => {
+    circle.style.height = `${inputRange.value}%`;
+    circle.style.width = `${inputRange.value}%`
+})
