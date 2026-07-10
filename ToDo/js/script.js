d2b04767c550991bc87498d794ae4452a39c4fca
xycu <smonindemid@gmail.com>
2026-07-10 13:32:00 +0500

    make a to-do list using localstorage

--- ToDo/js/script.js
+++ ToDo/js/script.js
@@ -3,20 +3,22 @@ const headerInput = document.querySelector('.header-input');
 const todoList = document.querySelector('.todo-list');
 const todoCompleted = document.querySelector('.todo-completed');
 
-const toDoData = [];
+const toDoData = JSON.parse(localStorage.getItem('task')) || [];
 
 
 function render() {
     todoList.innerHTML = '';
     todoCompleted.innerHTML = '';
     toDoData.forEach(item => {
+        
         const li = document.createElement('li');
         li.classList.add('todo-item');
-
+        
         li.innerHTML = `<span class="text-todo">${item.text}</span><div class="todo-buttons">
-					<button class="todo-remove"></button>
-					<button class="todo-complete"></button>
-				</div>`;
+        <button class="todo-remove"></button>
+        <button class="todo-complete"></button>
+        </div>`;
+        
 
         if (item.completed) {
             todoCompleted.append(li);
@@ -26,8 +28,10 @@ function render() {
 
         li.querySelector('.todo-complete').addEventListener('click', () => {
             item.completed = !item.completed;
+            localStorage.setItem('task', JSON.stringify(toDoData));
             render()
         })
+
         li.querySelector('.todo-remove').addEventListener('click', () => {
             const index = toDoData.indexOf(item);
 
@@ -35,10 +39,12 @@ function render() {
                 toDoData.splice(index, 1);
             }
 
+            localStorage.setItem('task', JSON.stringify(toDoData));
             render();
         })
     })
 }
+
 
 todoControl.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -53,8 +59,11 @@ todoControl.addEventListener('submit', (e) => {
     } else {
         toDoData.push(newTodo);
         headerInput.value = '';
-    
+        localStorage.setItem('task', JSON.stringify(toDoData));
+
         render();
     }
 
 })
+
+render();
