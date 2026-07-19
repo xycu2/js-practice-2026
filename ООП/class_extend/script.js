b66904b525d44ef39d528ee2d60878ca4344b739
xycu <smonindemid@gmail.com>
2026-07-19 14:13:06 +0500

    implement inheritance of the hello method and call of the parent method inside the Second class

--- ООП/class_extend/script.js
+++ ООП/class_extend/script.js
@@ -1,9 +1,12 @@
 class First {
     hello() {
-        console.log("Привет, я метод родителя!")
+        console.log("Привет, я метод родителя!");
     }
 }
 
 class Second extends First {
-
+    hello() {
+        super.hello();
+        console.log("А я наследуемый метод!");
+    }
 }
