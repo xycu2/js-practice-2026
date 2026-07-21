2c238b12eb0eb666e5c3004feb85a074be2d430e
xycu <smonindemid@gmail.com>
2026-07-21 20:03:44 +0500

    find elements on a page

---
+++ codePen/task3/script.js
@@ -0,0 +1,11 @@
+const country = document.getElementById('country');
+const city = document.getElementById('city');
+const result = document.querySelector('.result');
+
+const cityArr = {
+  rus: ['Москва', 'Санк-Петербург', 'Новосибирск', 'Екатеринбург', 'Нижний Новгород', 'Казань', 'Челябинск'],
+  uk: ['Киев', 'Харьков', 'Одесса', 'Днепр', 'Донецк', 'Запорожье', 'Львов'],
+  bel: ['Минск', 'Гомель', 'Могилёв', 'Витебск', 'Гродно', 'Брест'],
+  jap: ['Токио', 'Киото', 'Осака', 'Иокогама'] 
+}
+
