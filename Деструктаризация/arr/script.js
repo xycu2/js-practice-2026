46a2e9970303bced6e7d410b8e82447b7242e58e
xycu <smonindemid@gmail.com>
2026-07-22 12:41:24 +0500

    access nested properties of an array

--- Деструктаризация/arr/script.js
+++ Деструктаризация/arr/script.js
@@ -1,7 +1,12 @@
-const names = ['artem', 'max', 'vasya', 'petya', 'alex'];
+const names = ['artem', 'max', 'vasya', ['petya', 'alex']];
 
-const [art, max, vasya] = names;
+// получаем доступ к вложенным св-вам массива
+const [art, max, vasya, [petya, alex]] = names;
 
 console.log(art);
 console.log(max);
 console.log(vasya);
+
+// выводим их в консоль
+console.log(petya);
+console.log(alex);
