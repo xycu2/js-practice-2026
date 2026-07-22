8344b608d3a61e670f10849adcc5395fe0ae17e4
xycu <smonindemid@gmail.com>
2026-07-22 23:08:00 +0500

    getters and setters

--- ООП/getter_setter/script.js
+++ ООП/getter_setter/script.js
@@ -12,14 +12,24 @@ class Person {
 class FrontEndDev extends Person {
     constructor(name, age, skills = []) {
         super(name, age);
-        this.skills = skills;
+        this._skills = skills;
     }
 
+    // getter получает значение св-ва, "_" - приватное св-во , только чтение
     get skills() {
-        return this.skills;
+        return this._skills;
+    }
+
+    // setter записывает в св-во значение
+    set skills(str) {
+        this.skills.push(str)
     }
 }
 
 const dev = new FrontEndDev('Vlad', 23);
+
+dev.skills = 'Первое свойство';
+dev.skills = 'Второе свойство';
+dev.skills = 'Третье свойство';
 
 console.log(dev)
