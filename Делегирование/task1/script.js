99a9bb854e7a84779b60acba23d63d627eb9d29d
xycu <smonindemid@gmail.com>
2026-07-29 15:01:31 +0500

    implement color change in the card

--- Делегирование/task1/script.js
+++ Делегирование/task1/script.js
@@ -2,5 +2,8 @@ const previewBox = document.querySelector('.preview-box');
 const colorGrid = document.querySelector('.color-grid');
 
 colorGrid.addEventListener('click', (e) => {
-    
+    if (e.target.matches('.color-card')) {
+        const color = e.target.dataset.color;
+        previewBox.style.backgroundColor = color;
+    }
 })
