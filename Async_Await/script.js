082793e462419bc9f8d7ae1cf0567424e002bfb8
xycu <smonindemid@gmail.com>
2026-07-30 17:19:24 +0500

    async await practice

--- Async_Await/script.js
+++ Async_Await/script.js
@@ -1,5 +1,14 @@
-const getData = async () => {
-    return 'Alex';
-}
+const getData = async (id) => {
+  try {
+    const responseUsers = await fetch("./user.json");
+    const users = await responseUsers.json();
+    const responseUser = await fetch(`./${users[id].id}.json`);
+    return await responseUser.json();
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
 
-getData().then(data => console.log(data))
+getData(1)
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error.message));
